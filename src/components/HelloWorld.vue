18bd0cbc8c3556127a502f4363319712fe6ab050
joren97x <babamyloves2002@gmail.com>
2023-09-04 19:20:45 +0800

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,46 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
-</template>
+<script setup>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    import emailjs from '@emailjs/browser'
+    import {reactive} from 'vue'
+
+    const form = reactive({
+        sendername: '',
+        to: '',
+        subject: '',
+        replyto: '',
+        message: ''
+    })
+
+    // const sendername = ref('')
+    // const to = ref('')
+    // const subject = ref('')
+    // const replyto = ref('')
+    // const message = ref('')
+
+    const send = () => {
+        emailjs.send('service_kfsphbh', 'template_xzp03ja', 
+        {
+            sendername: form.sendername,
+            to: form.to,
+            subject: form.subject,
+            replyto: form.replyto,
+            message: form.message
+        }
+        , 'eEt-YCYeYc0LoTRxJ').then(() => {
+            alert('success?')
+        })
+    }
+
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<template> 
+    <v-container>
+        <p class="text-h1 text-center mb-3">Send email</p>
+        {{ form }}
+        <v-text-field variant="outlined" label="Sender name" v-model="form.sendername"></v-text-field>
+        <v-text-field variant="outlined" label="To" v-model="form.to"></v-text-field>
+        <v-text-field variant="outlined" label="Subject" v-model="form.subject"></v-text-field>
+        <v-text-field variant="outlined" label="Reply to" v-model="form.replyto"></v-text-field>
+        <v-textarea variant="outlined" label="Message" v-model="form.message"></v-textarea>
+        <v-btn color="blue" @click="send">Send</v-btn>
+    </v-container>
+</template>
